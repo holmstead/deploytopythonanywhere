17df185baed328c91e3f1f16014c18377f58d85c
comput <mat.holmes@outlook.com>
2025-05-12 20:33:00 +0100

    final push

--- dbconfig_pa.py
+++
@@ -1,6 +0,0 @@
-db_config = {
-    "host": "holmstead.mysql.pythonanywhere-services.com",
-    "user": "holmstead",
-    "password": "neoMorpheus",
-    "database": "holmstead$recipes"
-}
--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from recipeDAO import RecipeDAO # imports the DAO
 
-# create an instance of the DAO
-recipe_dao = RecipeDAO()  
+# Create an instance of the DAO
+recipe_dao = RecipeDAO()
 
-app = Flask(__name__, static_url_path='', static_folder='static') # Flask constructor
+# Flask constructor
+app = Flask(__name__, static_url_path='', static_folder='static') 
 # makes static files accessible at the root URL (/) instead of /static
 
 # Bind the index() funtion to the ‘/’ URL (homepage) 
@@ -13,26 +14,29 @@ def index():
     # Render the template 'index.html' from the 'templates' folder
     return render_template('index.html')
 
-# curl "http://127.0.0.1:5000/recipes" 
+# Endpoint: UGet all recipes by ID (PUT request)
 @app.route('/recipes', methods=['GET']) # Action: Get
 def getall():
-    # Fetch all recipes from the DAO
+    # Use get_all method from the DAO
     recipes = recipe_dao.get_all()
-    # Render the recipes template with the recipes data
+
+    # Return recipe as JSON
     return jsonify(recipes)
 
-
-# curl "http://127.0.0.1:5000/recipes/1"
+# Endpoint: Get recipe by ID (GET request)
 @app.route('/recipes/<int:id>', methods=['GET']) # Action: Get. <int:id> is a variable, URL looks like ~/recipes/1
-def get_recipe_by_id(id):
+def get_recipe_by_id(id):    
+    # Use find_by_id method in DAO to fetch recipe
     recipe = recipe_dao.find_by_id(id)
+    
     if recipe:
-        return jsonify(recipe)  # Return recipe as JSON
+        # Return recipe as JSON
+        return jsonify(recipe)  
     else:
+        # If no recipe found give 404 not found error
         return jsonify({"error": "Recipe not found"}), 404
 
-
-# curl "http://127.0.0.1:5000/recipes"
+# Endpoint: Create recipe (POST request)
 @app.route('/add_recipe', methods=['POST'])  # Action: Create
 def create_recipe():
     # Read JSON data from the body
@@ -45,10 +49,10 @@ def create_recipe():
     # Create the recipe using the DAO
     new_recipe = recipe_dao.create(json_data['name'], json_data['ingredients'], json_data['instructions'])
 
-    # Return the created recipe with a 201 status code
+    # Return the created recipe as JSON with a 201 status code (successfully created)
     return jsonify(new_recipe), 201
 
-
+# Endpoint: Update recipe by ID (PUT request)
 @app.route('/recipes/<int:id>', methods=['PUT'])  # Action: Update
 def update_recipe(id):
     # Read JSON data from the request body
@@ -67,22 +71,23 @@ def update_recipe(id):
     updated_recipe = recipe_dao.update(id, name, ingredients, instructions)
 
     if updated_recipe:
-        # Return the updated recipe with a 200 status code
+        # Return the updated recipe with a 200 status code (successful request)
         return jsonify(updated_recipe), 200  
     else:
-        # Return an error if the recipe was not found
+        # Return a 404 error if the recipe was not found
         return jsonify({"error": "Recipe not found or update failed."}), 404  
 
-
+# Endpoint: Delete recipe by ID (DELETE request)
 @app.route('/recipes/<int:id>', methods=['DELETE'])  # Action: Delete
 def delete_recipe(id):
     # Call the DAO to delete the recipe
     success = recipe_dao.delete(id)
+    
     if success:
-        # Return a success message with a 200 status code
+        # Return a success message with a 200 status code (successful request)
         return jsonify({"message": "Recipe deleted successfully."}), 200
     else:
-         # Return an error if the recipe was not found
+         # Return a 404 error if the recipe was not found
         return jsonify({"error": "Recipe not found."}), 404 
 
 
